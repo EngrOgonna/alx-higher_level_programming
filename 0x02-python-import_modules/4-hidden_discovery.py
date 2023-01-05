de0cbcd5b251b28064cb298df6aff6822f8c42d6
Ogonna <Goddeyalaba11@gmail.com>
2023-01-05 05:04:26 -0800

    ok

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,6 @@
-#!/usr/bin/python3.4
+#!/usr/bin/python3
 if __name__ == "__main__":
-    from hidden_4 import *
-    for s in dir():
+    import hidden_4
+    for s in dir(hidden_4):
         if s[:2] != "__":
-            print("{}".format(s))
+            print("{:s}".format(s))
